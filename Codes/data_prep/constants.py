07406ad0a8041144744a419c3e42dc0ad79b269a
KevinJCHe <kevinjc.he@gmail.com>
2024-11-11 14:12:16 -0500

    remove demo_cols

--- Codes/data_prep/constants.py
+++ Codes/data_prep/constants.py
@@ -1,8 +1,3 @@
-demo_cols = [
-    'height',
-    'weight',
-    'body_surface_area',
-]
 DROP_CLINIC_COLUMNS = [
     'MRN','Lab Type', 'Collected Date', 'Result Date', 'Finalized Date', 
     'Last Update', 'Accession', 'Order ID','Specimen Source', 'Specimen Type',
