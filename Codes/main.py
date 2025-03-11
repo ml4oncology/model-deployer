2da0904afbb507ac66492e94ff909caefb830ab5
KevinJCHe <kevinjc.he@gmail.com>
2025-03-11 18:26:28 -0400

    added loader.py, load all config and models files in a central place

--- Codes/main.py
+++ Codes/main.py
@@ -6,6 +6,7 @@ from tqdm import tqdm
 from collections import defaultdict
 
 from data_prep.final_processing import final_process
+from loader import Config, Model
 from model_eval.inference import get_ED_visit_model_output
 
 import warnings
@@ -32,36 +33,44 @@ if __name__ == "__main__":
     anchor = args.model_anchor
     ROOT_DIR = args.root_dir
 
-    # TODO: maybe we should only make data-dir and model-dir and info-dir into CLI arguments and remove root-dir? 
-    #       for better generalizability for end-users
-    data_dir = f'{ROOT_DIR}/Data' #Data
+    data_dir = f'{ROOT_DIR}/Data'
+    fig_dir = f'{ROOT_DIR}/Figures'
+    results_output = f"{anchor}_output.csv" 
+    
     info_dir= f'{ROOT_DIR}/Infos'
     train_param_dir = f'{ROOT_DIR}/Infos/Train_Data_parameters'
     code_dir = f'{ROOT_DIR}/Codes' # TODO: load config.yaml here (the only time code_dir is used)
     model_dir = f'{ROOT_DIR}/Models' 
-    fig_dir = f'{ROOT_DIR}/Figures'
-    results_output = f"{anchor}_output.csv" 
     
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
-    
-    date_range = pd.date_range(start_date, end_date, freq='d').strftime("%Y%m%d")
-    
-    thresholds = pd.read_excel(f'{info_dir}/ED_Prediction_Threshold.xlsx')
+
+    config = Config(info_dir=f'{ROOT_DIR}/Infos')
+    model = Model(
+        model_dir=f'{ROOT_DIR}/Models', 
+        prep_dir=f'{ROOT_DIR}/Infos/Train_Data_parameters', 
+        anchor=anchor
+    )
+
+    # Get pre-defined prediction thresholds
+    thresholds = config.thresholds
+    thresholds = thresholds.query(f'Model_anchor == "{anchor.title()}-anchored"')
+    thresholds = thresholds.set_index('Labels')['Prediction_threshold']
     
     # treatment anchored files named as eg. AIM2REDUCE_hematology_20241104
     # clinic anchored files named as eg. AIM2REDUCE_hematology_weekly_20241104
     postfix_map = {'treatment': '', 'clinic': 'weekly_'}
-    results = defaultdict(list)
 
+    date_range = pd.date_range(start_date, end_date, freq='d').strftime("%Y%m%d")
+    results = defaultdict(list)
     for i, data_pull_date in tqdm(enumerate(date_range)): 
 
         print(f'**** Processing #{i}: {data_pull_date} *****')
     
         postfix = postfix_map[anchor]
-        chemo_file_treatment = f"{data_dir}/{proj_name}_chemo_{postfix}{data_pull_date}.csv"
-        diagnosis_file_treatment = f"{data_dir}/{proj_name}_diagnosis_{postfix}{data_pull_date}.csv"
-        if not pd.read_csv(chemo_file_treatment).empty and not pd.read_csv(diagnosis_file_treatment).empty:
+        chemo_file = f"{data_dir}/{proj_name}_chemo_{postfix}{data_pull_date}.csv"
+        diagnosis_file = f"{data_dir}/{proj_name}_diagnosis_{postfix}{data_pull_date}.csv"
+        if not pd.read_csv(chemo_file).empty and not pd.read_csv(diagnosis_file).empty:
             ######################### Data Processing ################################
             ##******************** ED **********************##
             # Process and prepare data
@@ -69,12 +78,9 @@ if __name__ == "__main__":
 
             ######################### Model Evaluation ################################        
             ##******************** ED **********************##
-            # Load pre-defined prediction thresholds
-            thresholds_treatment = thresholds[thresholds['Model_anchor']==f'{anchor.title()}-anchored']
-            thresholds_treatment = thresholds_treatment.set_index('Labels')['Prediction_threshold']
-            ED_treatment_result = get_ED_visit_model_output(prepared_data_ED, thresholds_treatment, model_dir, fig_dir, anchor)
+            ED_result = get_ED_visit_model_output(prepared_data_ED, thresholds, model_dir, fig_dir, anchor)
 
-            results[anchor].append(ED_treatment_result)
+            results[anchor].append(ED_result)
         else:
             print(f"No Patient {anchor.title()} Data for: {data_pull_date}")
     
